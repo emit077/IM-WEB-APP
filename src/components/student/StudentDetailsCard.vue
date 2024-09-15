19e5d97678ac98e39ba5f1bd3694f650396dc1ed
amit.sahu <emitsahu077@gmail.com>
2024-09-15 18:53:49 +0530

    IM last Production release ::Sun Sep 15 18:53:49 IST 2024

--- src/components/student/StudentDetailsCard.vue
+++ src/components/student/StudentDetailsCard.vue
@@ -43,11 +43,9 @@
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.PROFILE_STATUS }} :</label>
               <p class="text-1">
-                <span
-                  v-if="student_basic_data.profile_status"
-                  :class="'status-' + student_basic_data.profile_status.toLowerCase()"
-                  >{{ student_basic_data.profile_status }}</span
-                >
+                <span v-if="student_basic_data.profile_status"
+                  :class="'status-' + student_basic_data.profile_status.toLowerCase()">{{
+                    student_basic_data.profile_status }}</span>
               </p>
             </div>
           </v-col>
@@ -79,8 +77,7 @@
     <v-card flat class="mb-5 pb-3" color="">
       <v-card-title class="primary--text text-body-1 font-weight-bold">
         <v-icon color="primary" class="mr-1" size="20">mdi-contacts</v-icon>
-        {{ $lang.CONTACT_DETAILS }}</v-card-title
-      >
+        {{ $lang.CONTACT_DETAILS }}</v-card-title>
       <div class="px-5">
         <v-divider class="card-divider" color="secondary"></v-divider>
       </div>
@@ -95,7 +92,7 @@
           <v-col class="py-0" cols="12" md="3">
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.WA_MOBILE_NUMBER }} :</label>
-              <p class="text-1">{{ mobile }}</p>
+              <p class="text-1">{{ student_basic_data.whatsapp_number }}</p>
             </div>
           </v-col>
           <v-col class="py-0" cols="12" md="3">
@@ -143,12 +140,7 @@
             </div>
           </v-col>
 
-          <v-col
-            class="py-0"
-            cols="12"
-            md="3"
-            v-if="student_basic_data.contact_person == 'Other'"
-          >
+          <v-col class="py-0" cols="12" md="3" v-if="student_basic_data.contact_person == 'Other'">
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.CONTACT_PERSON }} Name :</label>
               <p class="text-1">
@@ -156,12 +148,7 @@
               </p>
             </div>
           </v-col>
-          <v-col
-            class="py-0"
-            cols="12"
-            md="3"
-            v-if="student_basic_data.contact_person == 'Other'"
-          >
+          <v-col class="py-0" cols="12" md="3" v-if="student_basic_data.contact_person == 'Other'">
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.CONTACT_PERSON }} Mobile :</label>
               <p class="text-1">
@@ -252,10 +239,8 @@
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.SUBJECTS }} :</label>
               <p class="text-1">
-                <span v-for="(subject, i) in student_basic_data.subjects_list" :key="i"
-                  >{{ subject }}
-                  <span v-if="i >= 0 && i + 1 < student_basic_data.subjects_list.length"
-                    >,
+                <span v-for="(subject, i) in student_basic_data.subjects_list" :key="i">{{ subject }}
+                  <span v-if="i >= 0 && i + 1 < student_basic_data.subjects_list.length">,
                   </span>
                 </span>
               </p>
@@ -306,21 +291,13 @@
           <v-col class="py-0" cols="12" md="3" sm="6">
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.TEACHING_PREFERENCE }} :</label>
-              <p
-                class="text-1"
-                v-if="
-                  student_basic_data.teaching_preference &&
-                  student_basic_data.teaching_preference.length > 0
-                "
-              >
-                <span
-                  v-for="(item, i) in student_basic_data.teaching_preference"
-                  :key="item.id"
-                >
+              <p class="text-1" v-if="
+                student_basic_data.teaching_preference &&
+                student_basic_data.teaching_preference.length > 0
+              ">
+                <span v-for="(item, i) in student_basic_data.teaching_preference" :key="item.id">
                   {{ item }}
-                  <span v-if="student_basic_data.teaching_preference.length > i + 1"
-                    >,</span
-                  >
+                  <span v-if="student_basic_data.teaching_preference.length > i + 1">,</span>
                 </span>
               </p>
               <p class="text-1" v-else>-</p>
@@ -330,11 +307,7 @@
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.SUITABLE_DAYS }} :</label>
               <p class="text-1">
-                <span
-                  class="text-1"
-                  v-for="(item, i) in student_basic_data.suitable_days"
-                  :key="item.id"
-                >
+                <span class="text-1" v-for="(item, i) in student_basic_data.suitable_days" :key="item.id">
                   {{ item
                   }}<span v-if="i + 1 < student_basic_data.suitable_days.length">,</span>
                 </span>
@@ -346,26 +319,16 @@
             <div class="field-wrapper">
               <label class="label-text-1">{{ $lang.SUITABLE_TIME_SLOT }} :</label> <br />
               <span v-for="item in morning_time_slots" :key="item.id">
-                <span
-                  class="text-1"
-                  v-if="student_basic_data.time_slots_id_list.includes(item.id)"
-                >
+                <span class="text-1" v-if="student_basic_data.time_slots_id_list.includes(item.id)">
                   {{ item.slot_from + " - " + item.slot_to }} ,
                 </span>
               </span>
               <span v-for="item in afternoon_time_slots" :key="item.id">
-                <span
-                  class="text-1"
-                  v-if="student_basic_data.time_slots_id_list.includes(item.id)"
-                >
-                  {{ item.slot_from + " - " + item.slot_to }} ,</span
-                >
+                <span class="text-1" v-if="student_basic_data.time_slots_id_list.includes(item.id)">
+                  {{ item.slot_from + " - " + item.slot_to }} ,</span>
               </span>
               <span v-for="item in evening_time_slots" :key="item.id">
-                <span
-                  class="text-1"
-                  v-if="student_basic_data.time_slots_id_list.includes(item.id)"
-                >
+                <span class="text-1" v-if="student_basic_data.time_slots_id_list.includes(item.id)">
                   {{ item.slot_from + " - " + item.slot_to }} ,
                 </span>
               </span>
